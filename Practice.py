403257e3c4386b500af0eac9079c9db044dafc30
Anil Jujjuri <anil.kumarjujjuri@ltts.com>
2023-11-08 05:05:30 +0530

    new

--- Practice.py
+++ Practice.py
@@ -1,41 +1,34 @@
 from opcua import Client
+from opcua import ua
 
 # Specify the OPC UA server endpoint
 opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
 
-# Function to recursively explore and retrieve data from nodes
-def retrieve_node_hierarchy(node, depth=0):
-    print(node)
-    try:
-        # Connect to the OPC UA server
-        with Client(opcua_endpoint) as client:
-            client.connect()
+# Function to determine the type of a node
+def get_node_type(node):
+    if node.get_node_class() == ua.NodeClass.Object:
+        value = node.get_children()
+        print("the case1",value)
+    elif node.get_node_class() == ua.NodeClass.Variable:
+        value = node.get_value()
+        #print(f"Node Name: {node.get_browse_name().Name}, NodeId: {node.nodeid}, Value: {value}")
+        return "Variable"
+    elif node.get_node_class() == ua.NodeClass.Folder:
+        return "Folder"
+    else:
+        return "Unknown"  # You can extend this for other node types
 
-            # Read the value of the current node
-            value = node.get_value()
-            print(f"{'  ' * depth}Node: {node.get_browse_name()}. Value: {value}")
+# Read nodes from the CSV file
+with open('nodes.csv', 'r') as csvfile:
+    for row in csvfile:
+        node_id = row.strip()  # Assuming each line in the CSV contains a node ID
 
-            # If the node has children, explore them
-            children = node.get_children()
-            for child in children:
-                retrieve_node_hierarchy(child, depth + 1)
-    except Exception as e:
-        print(f"Error: {str(e)}")
+        try:
+            with Client(opcua_endpoint) as client:
+                client.connect()
+                node = client.get_node(node_id)
 
-try:
-    # Create a client connection
-    client = Client(opcua_endpoint)
-    client.connect()
-
-    # Get the root node using the specified node ID
-    root_node = "ns=0;i=85"
-    retrieve_node_hierarchy(root_node)
-
-except Exception as e:
-    print(f"Error connecting to the OPC UA server: {str(e)}")
-
-# Ensure to disconnect from the OPC UA server at the end
-client.disconnect()
-
-ns=0;i=85
-Error: 'str' object has no attribute 'get_value'
+                node_type = get_node_type(node)
+                print(f"{node_id} is a {node_type}")
+        except Exception as e:
+            print(f"Error reading node {node_id}: {str(e)}")
