4ccf92923e4127101634cd0ca6e387829d256e8c
AnilJujjuri007 <108789174+AnilJujjuri007@users.noreply.github.com>
2023-11-07 10:46:19 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,4 +1,5 @@
 from opcua import Client
+from opcua.common.results import Good, BadNoMatch
 
 # Specify the OPC UA server endpoint
 opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
@@ -7,13 +8,17 @@ opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
 def retrieve_node_hierarchy(node, depth=0):
     try:
         # Read the value of the current node
-        value = node.get_value()
-        print(f"{'  ' * depth}Node: {node.get_browse_name()}. Value: {value}")
+        value_attribute = node.get_attributes(["Value"])
+        if value_attribute and value_attribute[0].Value.Status == Good:
+            value = value_attribute[0].Value.Value
+            print(f"{'  ' * depth}Node: {node.get_browse_name()}. Value: {value}")
 
         # If the node has children, explore them
         children = node.get_children()
         for child in children:
             retrieve_node_hierarchy(child, depth + 1)
+    except BadNoMatch:
+        print(f"Node: {node.get_browse_name()} does not have a 'Value' attribute.")
     except Exception as e:
         print(f"Error: {str(e)}")
 
