176ebec5e3f4b0f946a4892e5d6897669c1daf9e
AnilJujjuri007 <108789174+AnilJujjuri007@users.noreply.github.com>
2023-11-07 10:43:35 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -5,20 +5,15 @@ opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
 
 # Function to recursively explore and retrieve data from nodes
 def retrieve_node_hierarchy(node, depth=0):
-    print(node)
     try:
-        # Connect to the OPC UA server
-        with Client(opcua_endpoint) as client:
-            client.connect()
+        # Read the value of the current node
+        value = node.get_value()
+        print(f"{'  ' * depth}Node: {node.get_browse_name()}. Value: {value}")
 
-            # Read the value of the current node
-            value = node.get_value()
-            print(f"{'  ' * depth}Node: {node.get_browse_name()}. Value: {value}")
-
-            # If the node has children, explore them
-            children = node.get_children()
-            for child in children:
-                retrieve_node_hierarchy(child, depth + 1)
+        # If the node has children, explore them
+        children = node.get_children()
+        for child in children:
+            retrieve_node_hierarchy(child, depth + 1)
     except Exception as e:
         print(f"Error: {str(e)}")
 
@@ -27,8 +22,9 @@ try:
     client = Client(opcua_endpoint)
     client.connect()
 
-    # Get the root node using the specified node ID
-    root_node = "ns=0;i=85"
+    # Get the root node object
+    root_node = client.get_node("ns=0;i=85")
+
     retrieve_node_hierarchy(root_node)
 
 except Exception as e:
@@ -36,6 +32,3 @@ except Exception as e:
 
 # Ensure to disconnect from the OPC UA server at the end
 client.disconnect()
-
-ns=0;i=85
-Error: 'str' object has no attribute 'get_value'
