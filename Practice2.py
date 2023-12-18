62d341104fa5c0a994d272faaac58d13e4dae55c
Anil Jujjuri <anil.kumarjujjuri@ltts.com>
2023-12-18 09:46:04 +0530

    new

--- Practice2.py
+++ Practice2.py
@@ -1,51 +1,73 @@
-import tkinter as tk
-from tkinter import filedialog
+from flask import Flask, request, jsonify, render_template
 from opcua import Client
 from opcua import ua
+import os
+
+app = Flask(__name__)
 
 # Specify the OPC UA server endpoint
-opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
+# opcua_endpoint = "opc.tcp://BLRTSL00330.lnties.com:53530/OPCUA/SimulationServer"
 
-# Function to determine the type of a node
-def get_node_type(node):
-    if node.get_node_class() == ua.NodeClass.Object:
-        children = node.get_children()
-        print(f"{node.get_browse_name()} is an object having following child nodes:")
-        for child in children:
-            node_type = get_node_type(child)
+# Function to determine the type of a node and retrieve its value
+def get_node_type_and_value(client, node):
+    try:
+        if node.get_node_class() == ua.NodeClass.Object:
+            children = node.get_children()
+            result = {"object": str(node.get_browse_name()), "children": []}
+            for child in children:
+                result["children"].append(get_node_type_and_value(client, child))
+            return result
+        elif node.get_node_class() == ua.NodeClass.Variable:
+            value = node.get_value()
+            result = {
+                "node_name": node.get_browse_name().Name,
+                "node_id": str(node.nodeid),
+                "value": str(value) if value is not None else None,
+            }
+            return result
+    except Exception as e:
+        return("err",e)
 
-    elif node.get_node_class() == ua.NodeClass.Variable:
-        value = node.get_value()
-        print(f"Node Name: {node.get_browse_name().Name}, NodeId: {node.nodeid}, Value: {value}")
-
-# Function to upload and process a CSV file
-def upload_and_process_csv():
-    file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-    if file_path:
-        try:
-            with Client(opcua_endpoint) as client:
-                client.connect()
-                with open(file_path, 'r') as csvfile:
-                    for row in csvfile:
-                        node_id = row.strip()
-                        try:
+# Function to process a CSV file
+def process_csv(file_path):
+    results = []
+    try:
+        with open(file_path, 'r') as csvfile:
+            for row in csvfile:
+                node_info = row.strip().split(',')
+                if len(node_info) == 2:
+                    node_id, connection_address = node_info
+                    try:
+                        with Client(connection_address) as client:
+                            client.connect()
                             node = client.get_node(node_id)
-                            node_type = get_node_type(node)
-                            print(f"{node_id} is a {node_type}")
-                        except Exception as e:
-                            print(f"Error reading node {node_id}: {str(e)}")
-                # Disconnect the OPC UA client after processing
-                client.disconnect()
-        except Exception as e:
-            print(f"Error connecting to the OPC UA server: {str(e)}")
+                            result = get_node_type_and_value(client, node)
+                            results.append(result)
+                    except Exception as e:
+                        error_msg = f"Error reading node {node_id} from {connection_address}: {str(e)}"
+                        results.append({"error": error_msg})
+    except Exception as e:
+        error_msg = f"Error processing CSV file: {str(e)}"
+        results.append({"error": error_msg})
+    return results
 
-# Create a Tkinter window
-window = tk.Tk()
-window.title("CSV File Upload and OPC UA Processing")
+# API endpoint to process CSV and retrieve OPC UA data
+@app.route('/process_opcua_csv', methods=['POST'])
+def process_opcua_csv():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"})
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
+    file.save("uploaded_file.csv")
+    csv_file_path = "uploaded_file.csv"
+    results = process_csv(csv_file_path)
+    return jsonify({"results": results})
 
-# Create a button to trigger the file upload and processing
-upload_button = tk.Button(window, text="Upload CSV File and Process", command=upload_and_process_csv)
-upload_button.pack()
+# Route to render the file upload form
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-# Start the Tkinter main loop
-window.mainloop()
+if __name__ == '__main__':
+    app.run(debug=True)
